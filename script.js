ce0946338611a2d34bba7514f210c40386c289d8
Syam Pushpan <60807948+syam1245@users.noreply.github.com>
2023-10-12 13:32:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let URLhost = window.screen;
+console.log(URLhost);
+
+let URLheight = window.screen;
+console.log(URLheight.height);
+
+let URLwidth = window.screen;
+console.log(URLwidth.width);
+
+let URL = window.location;
+console.log(URL.href);
+
+let URLhostname = window.location;
+console.log(URLhostname.hostname);
+
+function goBack() {
+  window.history.back();
+}
+
+function goForward() {
+  window.history.forward();
+}
+
+
+
